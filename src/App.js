2adae9a055bafed629b0df0ba61a739647c3bf92
Dan Nguyen <nguyennamdanbk2610@gmail.com>
2019-06-12 11:24:41 +0700

    commitfirst

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import lottie from 'lottie-web'
+
+import animation from './static/data 5.json'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {show: false}
+  }
+
+  componentDidUpdate() {
+    if (this._el) {
+      this._animation = lottie.loadAnimation({
+        container: this._el,
+        renderer: 'svg',
+        animationData: animation,
+      })
+    }
+  }
+
+  _click = () => {
+    this.setState({show: true})
+
+    setTimeout(() => this.setState({show: false}), 1300)
+  }
+
+  render() {
+    return (
+      <div>
+        <p>
+          <span
+            style={{
+              fontWeight: 'bold',
+              color: 'purple',
+              cursor: 'pointer',
+            }}
+            onClick={this._click}
+          >
+            Click me
+          </span>
+          {' to see miracle'}
+        </p>
+        <p>Now go build something great.</p>
+        {this.state.show && (
+          <div
+            id="animation"
+            ref={el => (this._el = el)}
+            style={{
+              position: 'absolute',
+              width: '100%',
+              height: '100%',
+              top: 0,
+              left: 0,
+            }}
+          />
+        )}
+      </div>
+    )
+  }
+}
+
+export default App
